84f641d1d7a357d5764c9b07b6b104fcf046a8d4
Kobe Lipkens <kobelipkens@gmail.com>
2017-01-18 15:22:18 +0100

    Extend twig base template in order to: - dispatch EntityPresentedEvent - add drupalish twig debug html comments

---
+++ src/Twig/Template.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\wmcontroller\Twig;
+
+use Drupal\wmcontroller\Event\EntityPresentedEvent;
+
+abstract class Template extends \Twig_Template
+{
+    protected static $dispatched = [];
+
+    public function display(array $context, array $blocks = array())
+    {
+        if ($this->env->isDebug()) {
+            print('<!-- TWIG DEBUG -->');
+            printf('<!-- Template: %s -->', $this->getTemplateName());
+        }
+
+        foreach ($context as $var) {
+            if (!($var instanceof \Drupal\Core\Entity\EntityInterface)) {
+                continue;
+            }
+
+            $key = sprintf('%s:%s', $var->getEntityTypeId(), $var->id());
+            if (isset(self::$dispatched[$key])) {
+                continue;
+            }
+
+            self::$dispatched[$key] = true;
+            \Drupal::service('event_dispatcher')->dispatch(
+                'entity.presented',
+                new EntityPresentedEvent($var)
+            );
+        }
+
+        return parent::display($context, $blocks);
+    }
+}
+
