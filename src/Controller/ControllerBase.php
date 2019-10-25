18b11d6e62c8bf191b6fc8ee100b780daf549da5
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-10-25 12:37:04 +0200

    Stop extending the core ControllerBase
    + add StringTranslationTrait and a redirect helper

--- src/Controller/ControllerBase.php
+++ src/Controller/ControllerBase.php
@@ -2,11 +2,15 @@
 
 namespace Drupal\wmcontroller\Controller;
 
-use Drupal\Core\Controller\ControllerBase as DrupalControllerBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
 use Drupal\wmcontroller\ViewBuilder\ViewBuilder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-abstract class ControllerBase extends DrupalControllerBase
+abstract class ControllerBase
 {
+    use StringTranslationTrait;
+
     protected $templateDir = '';
 
     /**
@@ -22,6 +26,31 @@ abstract class ControllerBase extends DrupalControllerBase
             ->setTemplateDir($this->templateDir)
             ->setData($data)
             ->setTemplate($template);
+    }
+
+    /**
+     * Returns a redirect response object for the specified route.
+     *
+     * @param string $routeName
+     *   The name of the route to which to redirect.
+     * @param array $routeParameters
+     *   (optional) Parameters for the route.
+     * @param array $options
+     *   (optional) An associative array of additional options.
+     * @param int $status
+     *   (optional) The HTTP redirect status code for the redirect. The default is
+     *   302 Found.
+     *
+     * @return RedirectResponse
+     *   A redirect response object that may be returned by the controller.
+     */
+    protected function redirect($routeName, array $routeParameters = [], array $options = [], $status = 302)
+    {
+        $url = Url::fromRoute($routeName, $routeParameters, $options)
+            ->setAbsolute(true)
+            ->toString();
+
+        return new RedirectResponse($url, $status);
     }
 }
 
