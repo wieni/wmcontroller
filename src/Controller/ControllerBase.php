262b16775ff3a58ac33a79b72944e32edf9b20ae
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-04-28 11:13:16 +0200

    Inject ViewBuilder in ControllerBase

--- src/Controller/ControllerBase.php
+++ src/Controller/ControllerBase.php
@@ -2,16 +2,29 @@
 
 namespace Drupal\wmcontroller\Controller;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 use Drupal\wmcontroller\ViewBuilder\ViewBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-abstract class ControllerBase
+abstract class ControllerBase implements ContainerInjectionInterface
 {
     use StringTranslationTrait;
 
+    /** @var ViewBuilder */
+    protected $viewBuilder;
+    /** @var string */
     protected $templateDir = '';
+
+    public static function create(ContainerInterface $container)
+    {
+        $instance = new static;
+        $instance->viewBuilder = $container->get('wmcontroller.viewbuilder');
+
+        return $instance;
+    }
 
     /**
      * Return a new view from the application.
@@ -22,7 +35,7 @@ abstract class ControllerBase
      */
     protected function view($template = '', $data = [])
     {
-        return \Drupal::service('wmcontroller.viewbuilder')
+        return $this->viewBuilder
             ->setTemplateDir($this->templateDir)
             ->setData($data)
             ->setTemplate($template);
