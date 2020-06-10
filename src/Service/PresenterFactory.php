54e371a26851b5cfc3fa6f273fd49b4027a9b920
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-06-10 10:55:01 +0200

    Dispatch presented event in presenter factory

--- src/Service/PresenterFactory.php
+++ src/Service/PresenterFactory.php
@@ -2,25 +2,35 @@
 
 namespace Drupal\wmcontroller\Service;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\wmcontroller\Entity\HasPresenterInterface;
 use Drupal\wmcontroller\Entity\PresenterInterface;
+use Drupal\wmcontroller\Service\Cache\Dispatcher;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class PresenterFactory implements PresenterFactoryInterface
 {
     /** @var ContainerInterface */
     protected $container;
+    /** @var Dispatcher */
+    protected $dispatcher;
 
-    public function __construct(ContainerInterface $container)
-    {
+    public function __construct(
+        ContainerInterface $container,
+        Dispatcher $dispatcher
+    ) {
         $this->container = $container;
+        $this->dispatcher = $dispatcher;
     }
 
     public function getPresenterForEntity(HasPresenterInterface $entity): PresenterInterface
     {
         $presenter = $this->container->get($entity->getPresenterService());
-
         $presenter->setEntity($entity);
+
+        if ($entity instanceof EntityInterface) {
+            $this->dispatcher->dispatchPresented($entity);
+        }
 
         return $presenter;
     }
