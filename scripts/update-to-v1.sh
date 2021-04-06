b63bed1b3bb775aac4df6ad37b3b4a81feb219f4
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-04-06 11:51:33 +0200

    Add Bash script to help with migration

---
+++ scripts/update-to-v1.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+find $1 -type f -print0 | xargs -0 sed -i -r \
+  -e 's/Drupal\\wmcontroller\\Entity\\(AbstractPresenter|HasPresenterInterface|PresenterInterface)/Drupal\\wmpresenter\\Entity\\\1/g' \
+  -e 's/Drupal\\wmcontroller\\Twig\\Extension\\PresenterExtension/Drupal\\wmpresenter\\Twig\\Extension\\PresenterExtension/g' \
+  -e 's/Drupal\\wmcontroller\\Service\\(PresenterFactory(Interface)?)/Drupal\\wmpresenter\\\1/g' \
+  -e 's/wmcontroller\.presenter\.(.+)/wmpresenter.\1/g' \
+  -e 's/wmcontroller\.presenter/wmpresenter.presenter/g' \
+  -e 's/Drupal\\wmcontroller\\Service\\Cache\\(.+)/Drupal\\wmpage_cache\\\1/g' \
+  -e 's/wmcontroller\.cache\.(.+)/wmpage_cache.\1/g'
