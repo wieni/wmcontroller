8935bc9fcae0e777e21885bc9762bdca881c26e0
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-06-29 13:08:21 +0200

    Remove unused WmcontrollerEvents::PRESENTED constant

--- src/WmcontrollerEvents.php
+++ src/WmcontrollerEvents.php
@@ -5,15 +5,6 @@ namespace Drupal\wmcontroller;
 final class WmcontrollerEvents
 {
     /**
-     * Will be triggered whenever a variable is injected into
-     * a template.
-     *
-     * The event object is an instance of
-     * @see \Drupal\wmcontroller\Event\PresentedEvent
-     */
-    public const PRESENTED = 'item.presented';
-
-    /**
      * Will be triggered when an EntityInterface is being passed
      * to a controller. (i.e.: the main entity that is supposed to be rendered)
      *
