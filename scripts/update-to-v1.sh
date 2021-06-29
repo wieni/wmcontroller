fc86be3a7de9c3b56138e8253a08abdca73afaac
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-06-29 12:09:30 +0200

    Update Bash script to help with migration

--- scripts/update-to-v1.sh
+++ scripts/update-to-v1.sh
@@ -2,9 +2,10 @@
 
 find "$@" -type f -print0 | xargs -0 sed -i -r \
   -e 's/Drupal\\wmcontroller\\Entity\\(AbstractPresenter|HasPresenterInterface|PresenterInterface)/Drupal\\wmpresenter\\Entity\\\1/g' \
-  -e 's/Drupal\\wmcontroller\\Twig\\Extension\\PresenterExtension/Drupal\\wmpresenter\\Twig\\Extension\\PresenterExtension/g' \
+  -e 's/Drupal\\wmcontroller\\Service\\Cache\\(.+)/Drupal\\wmpage_cache\\\1/g' \
   -e 's/Drupal\\wmcontroller\\Service\\(PresenterFactory(Interface)?)/Drupal\\wmpresenter\\\1/g' \
+  -e 's/Drupal\\wmcontroller\\Twig\\Extension\\PresenterExtension/Drupal\\wmpresenter\\Twig\\Extension\\PresenterExtension/g' \
+  -e 's/Drupal\\wmcontroller\\ViewBuilder\\ViewBuilder/Drupal\\wmtwig\\ViewBuilder/g' \
   -e 's/wmcontroller\.presenter\.(.+)/wmpresenter.\1/g' \
   -e 's/wmcontroller\.presenter/wmpresenter.presenter/g' \
-  -e 's/Drupal\\wmcontroller\\Service\\Cache\\(.+)/Drupal\\wmpage_cache\\\1/g' \
   -e 's/wmcontroller\.cache\.(.+)/wmpage_cache.\1/g'
