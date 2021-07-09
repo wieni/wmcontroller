bad07adb6d58922928694ddc3144e8506d309ef7
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-07-09 14:53:43 +0200

    Update Bash script to help with migration

--- scripts/update-to-v1.sh
+++ scripts/update-to-v1.sh
@@ -2,7 +2,9 @@
 
 find "$@" -type f -print0 | xargs -0 sed -i -r \
   -e 's/Drupal\\wmcontroller\\Entity\\(AbstractPresenter|HasPresenterInterface|PresenterInterface)/Drupal\\wmpresenter\\Entity\\\1/g' \
+  -e 's/Drupal\\wmcontroller\\Entity\\Cache/Drupal\\wmpage_cache\\Cache/g' \
   -e 's/Drupal\\wmcontroller\\Service\\Cache\\(.+)/Drupal\\wmpage_cache\\\1/g' \
+  -e 's/Drupal\\wmcontroller\\Service\\Factory/Drupal\\wmpage_cache\\ServiceFactory/g' \
   -e 's/Drupal\\wmcontroller\\Service\\(PresenterFactory(Interface)?)/Drupal\\wmpresenter\\\1/g' \
   -e 's/Drupal\\wmcontroller\\Twig\\Extension\\PresenterExtension/Drupal\\wmpresenter\\Twig\\Extension\\PresenterExtension/g' \
   -e 's/Drupal\\wmcontroller\\ViewBuilder\\ViewBuilder/Drupal\\wmtwig\\ViewBuilder/g' \
